7785a0117a98b6cbf8fe63aeb3d888c4dbb03d38
Quanx0411 <quanx0411@gmail.com>
2026-08-24 15:48:12 +0900

    fix : 사진 찍으면 마우스 커서 생기면서 화면 돌리기 어려워지는 문제.... 단순 마우스 커서 안 나오기로수정

--- Source/NoPhotos/Gameplay/Photo/NPPhotoPreviewWidget.cpp
+++ Source/NoPhotos/Gameplay/Photo/NPPhotoPreviewWidget.cpp
@@ -11,7 +11,6 @@
 #include "NoPhotosGameState.h"
 #include "NoPhotosPlayerController.h"
 #include "TimerManager.h"
-#include "GameFramework/PlayerController.h"
 
 void UNPPhotoPreviewWidget::NativeConstruct()
 {
@@ -181,26 +180,6 @@ void UNPPhotoPreviewWidget::SetTransferNotificationVisible(const bool bVisible)
 		PhotoTransferText->SetVisibility(NotificationVisibility);
 	}
 
-	if (APlayerController* PlayerController = GetOwningPlayer())
-	{
-		if (bVisible)
-		{
-			PlayerController->bShowMouseCursor = true;
-
-			FInputModeGameAndUI InputMode;
-			InputMode.SetWidgetToFocus(TakeWidget());
-			InputMode.SetHideCursorDuringCapture(false);
-			InputMode.SetLockMouseToViewportBehavior(
-				EMouseLockMode::DoNotLock);
-
-			PlayerController->SetInputMode(InputMode);
-		}
-		else
-		{
-			PlayerController->bShowMouseCursor = false;
-			PlayerController->SetInputMode(FInputModeGameOnly());
-		}
-	}
 }
 
 void UNPPhotoPreviewWidget::HandleTransferRequestTimeout()
